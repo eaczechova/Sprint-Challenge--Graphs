5b7ddd5afc6929eb427cbcb893f32860cb6aff28
eaczechova <ewa.czech@hotmail.com>
2020-09-27 16:39:42 +0200

    passes test with 500 entries and 1000 moves, the adjacency dictionaries still has ?

--- adv.py
+++ adv.py
@@ -25,16 +25,120 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = {}
+            directions = player.current_room.get_exits()
+       
+            for d in directions:
+                graph.vertices[vertex_id][d] = '?'
+
+    def add_edge(self, vertex_id, key, value):
+        self.vertices[vertex_id][key] = value
+            
+    def __repr__(self):
+        return str(self.vertices)
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+
+# list of directions to follow
 traversal_path = []
 
-
-
-# TRAVERSAL TEST - DO NOT MODIFY
+# list of visited rooms
 visited_rooms = set()
-player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
+
+# instantiate Graph class
+graph = Graph()
+
+# instantiate Stack class
+stack = Stack()
+
+# Helper functions
+
+# function to find opposite direction 
+def reverse_direction(direction):
+    if direction == "n":
+        return "s"
+    elif direction == "s":
+        return "n"
+    elif direction == "e":
+        return "w"
+    else:
+        return "e"
+
+# additional function that draw new maze - to be fixed !!!
+def draw_graph(current_room, direction, next_room):
+
+    graph.add_vertex(current_room)
+    rev_direction = reverse_direction(direction)
+    graph.add_edge(current_room, direction, next_room)
+    graph.add_vertex(next_room)
+    graph.add_edge(next_room, rev_direction, current_room)
+
+# add first direction from current room direction array, e.g 'n', to traversal_path
+traversal_path = [player.current_room.get_exits()[0]]
+# add first direction from current room direction array, e.g 'n', to the stack
+stack.push([player.current_room.get_exits()[0]])
+
+
+# while stack is not empty we traverse the maze
+
+while stack.size() > 0:
+    # get direction from the end of the stack
+    move = stack.pop()
+    
+    ### for draw_graph()
+    cur_room = player.current_room.id
+    
+    # travel (current room changes)
+    player.travel(move)
+
+    ### for draw_graph()
+    n_room = player.current_room.id
+
+    # check if the current room has not been visited before
+    if player.current_room not in visited_rooms:
+        # append reverse_direction of taken direction to the traversal_path and stack
+        traversal_path.append(reverse_direction(move))
+        stack.push(reverse_direction(move))
+        # add room to visited to avoid going in circles
+        visited_rooms.add(player.current_room)
+    
+    # get all the direction from the current room 
+    room_directions = player.current_room.get_exits()
+    # loop over each direction player can go from the current room  
+    for direction in room_directions:
+        # set the next room for each direction
+        next_room = player.current_room.get_room_in_direction(direction)
+        # if the room has not been visited
+       
+        if next_room is not None and next_room not in visited_rooms:
+            # append direction to out traversal_path and stack
+            traversal_path.append(direction)
+            stack.push(direction)
+            draw_graph(cur_room, direction, n_room)
+            break
+
+
+print("Traversal_path:", traversal_path)
+print("New graph:", graph.vertices)
 
 for move in traversal_path:
     player.travel(move)
